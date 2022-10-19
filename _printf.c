a04791e75d2862484f51feb6d1d511de91213269
blessing-law <blessing.lawrence77@gmail.com>
2022-10-19 00:44:02 -0700

    late check

--- _printf.c
+++ _printf.c
@@ -1,44 +1,6 @@
 #include "main.h"
 
-<<<<<<< HEAD
 void print_buffer(char buffer[], int *buff_ind);
-=======
-/**
- * check_for_specifiers - checks if there is a valid format specifier
- * @format: possible format specifier
- *
- * Return: pointer to valid function or NULL
- */
-static int (*check_for_specifiers(const char *format))(va_list)
-{
-	unsigned int i;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
-
-	for (i = 0; p[i].t != NULL; i++)
-	{
-		if (*(p[i].t) == *format)
-		{
-			break;
-		}
-	}
-	return (p[i].f);
-}
->>>>>>> 04039754b831234ead0173a2e6e310a739479f8f
 
 /**
  *  _printf - Printf function
